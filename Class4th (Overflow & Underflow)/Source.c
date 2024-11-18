1ad7a34733a6788e23ecec6662eb5692f6ea2edf
Yg070212 <winter070212@gmail.com>
2024-11-18 19:46:18 +0900

    24-11-18 <조건문>

---
+++ Class4th (Overflow & Underflow)/Source.c
@@ -0,0 +1,98 @@
+#include <stdio.h> 
+
+void main()
+{
+#pragma region 오버플로우
+	// 특정한 자료형이 표현할 수 있는 최댓값의 범위를
+	// 넘어서 연산을 수행하는 과정입니다.
+
+	// char character = 129; // <- -128
+
+	// printf("character 변수의 값 : %d\n", character);
+
+	// 오버플로우는 부호 없는 자료형에서도 똑같이 발생하며,
+	// 실수일 때, 오버플로우가 발생하면 infinity라는 값이 출력됩니다.
+#pragma endregion
+
+#pragma region 언더플로우
+	// 특정한 자료형이 표현할 수 있는 최솟값의 범위
+	// 를 넘어서 연산을 수행하는 과정입니다.
+
+	// char alphabet = -130;
+
+	// printf("alphabet 변수의 값 : %d\n", alphabet);
+
+	// 언더플로우는 부호 없는 자료형에서 똑같이 발생
+	// 하며, 실수일 때 언더플로우가 발생하면 0이라는 값이 출력됩니다.
+#pragma endregion
+
+#pragma region 시프트 연산자
+	// 비트의 위치를 오른쪽 또는 왼쪽으로 특정한 수만큼
+	// 이동시키는 연산자입니다.
+
+	// int x = 10;
+	// int y = 12;
+
+	// printf("x의 값을 2번 왼쪽으로 이동한 결과 : %d\n", x << 2);
+	// printf("y의 값을 2번 오른쪽으로 이동한 결과 : %d\n", y >> 2);
+
+#pragma endregion
+
+#pragma region 산술 연산자
+
+	/*// 변수 + 변수
+
+	int x = 10;
+	int y = 5;
+
+	const int a = 20;
+	const int b = 15;
+
+	int result1; // 변수 + 변수
+	int result2; // 리터럴 상수 - 변수
+	int result3; // 심볼릭 상수 * 리터럴 상수
+	int result4; // 리터럴 상수 / 리터럴 상수
+	int result5; // 심볼릭 상수 % 심볼릭 상수
+
+	result1 = x + y;
+	result2 = y - x;
+	result3 = a * y;
+	result4 = x / y;
+	result5 = a % b;
+
+	printf("result1 의 연산 결과 : %d\n", result1);
+	printf("result2 의 연산 결과 : %d\n", result2);
+	printf("result3 의 연산 결과 : %d\n", result3);
+	printf("result4 의 연산 결과 : %d\n", result4);
+	printf("result5 의 연산 결과 : %d\n", result5); // <- by YG
+
+/*--------------------------------------------------------------*/
+
+/*int data = 10;
+const int constant = 10;
+
+int result1 = data + data;
+int result2 = 15 - data;
+int result3 = constant * 5;
+int result4 = 15 / 15;
+int result5 = constant * constant;
+
+printf("result1 변수의 값 : %d\n", result1);
+printf("result2 변수의 값 : %d\n", result2);
+printf("result3 변수의 값 : %d\n", result3);
+printf("result4 변수의 값 : %d\n", result4);
+printf("result5 변수의 값 : %d\n", result5); // <- KGS_T */
+
+#pragma endregion
+
+#pragma region 부호 없는 자료형
+
+/*unsigned short mineral = 65535; // short 자료형의 값의 범위는 65534이지만 unsigned를 사용하면 범위를 더 사용 가능함.
+unsigned int gas = -1;
+
+printf("mineral 변수의 값 : %u\n", mineral);
+
+printf("gas 변수의 값 : %u\n", gas);
+printf("gas 변수의 값 : %d\n", gas);*/
+#pragma endregion
+}
--- Program/Program.c
+++ Program/Program.c
@@ -1,98 +1,135 @@
 #include <stdio.h> 
 
-void main()
-{
-#pragma region 오버플로우
-	// 특정한 자료형이 표현할 수 있는 최댓값의 범위를
-	// 넘어서 연산을 수행하는 과정입니다.
+#pragma region 전처리기
+// 프로그램이 컴파일되기 이전에 프로그램에 대한
+// 사전 처리하는 과정입니다.
 
-	// char character = 129; // <- -128
+// 전처리기는 컴파일러가 아닌 선행처리기에 의해서
+// 처리되는 문장이기 때문에 명령문 끝에 ";"을 사용하지 않습니다.
+#pragma endregion
+
+void main(){
+#pragma region 실수를 저장하는 방법
 	
-	// printf("character 변수의 값 : %d\n", character);
+#pragma region 고정 소수점
+	// 소수점의 위치를 고정하여 소수점의 위치를
+	// 나타내는 방법입니다.
 
-	// 오버플로우는 부호 없는 자료형에서도 똑같이 발생하며,
-	// 실수일 때, 오버플로우가 발생하면 infinity라는 값이 출력됩니다.
+	// 고정 소수점은 정수부와 소수부의 크기가 작으므로
+	// 표현할 수 있는 범위가 한정되어 있습니다.
 #pragma endregion
 
-#pragma region 언더플로우
-	// 특정한 자료형이 표현할 수 있는 최솟값의 범위
-	// 를 넘어서 연산을 수행하는 과정입니다.
+#pragma region 부동 소수점
+	// 소수점의 위치를 고정하지 않고 소수점의 위치를
+	// 나타내는 방법입니다.
 
-	// char alphabet = -130;
-
-	// printf("alphabet 변수의 값 : %d\n", alphabet);
-
-	// 언더플로우는 부호 없는 자료형에서 똑같이 발생
-	// 하며, 실수일 때 언더플로우가 발생하면 0이라는 값이 출력됩니다.
+	// 가수부에 있는 비트 중에서 가장 왼쪽에 있는 수는
+	// 표현하지 않습니다.
 #pragma endregion
 
-#pragma region 시프트 연산자
-	// 비트의 위치를 오른쪽 또는 왼쪽으로 특정한 수만큼
-	// 이동시키는 연산자입니다.
+#pragma region 실수 형태의 (10)진수를 (2)진수로 변환하는 과정
+	// 10진수 실수 부분을 1.0이 될 때까지 계속 2로 곱한
+	// 다음 곱한 결과의 정수 부분을 위에서 아래로 순서대로
+	// 정렬합니다.
 
-	// int x = 10;
-	// int y = 12;
+	// float packet = 3.125f;
+	// float pi = 3.141592f;
+	// 
+	// printf("packet 변수의 값 : %f\n", packet);
+	// printf("pi 변수의 값 : %.8f\n", pi);
 
-	// printf("x의 값을 2번 왼쪽으로 이동한 결과 : %d\n", x << 2);
-	// printf("y의 값을 2번 오른쪽으로 이동한 결과 : %d\n", y >> 2);
-
-#pragma endregion
-
-#pragma region 산술 연산자
-
-	/*// 변수 + 변수
-
-	int x = 10;
-	int y = 5;
-
-	const int a = 20;
-	const int b = 15;
-
-	int result1; // 변수 + 변수
-	int result2; // 리터럴 상수 - 변수
-	int result3; // 심볼릭 상수 * 리터럴 상수
-	int result4; // 리터럴 상수 / 리터럴 상수
-	int result5; // 심볼릭 상수 % 심볼릭 상수
-
-	result1 = x + y;
-	result2 = y - x;
-	result3 = a * y;
-	result4 = x / y;
-	result5 = a % b;
-
-	printf("result1 의 연산 결과 : %d\n", result1);
-	printf("result2 의 연산 결과 : %d\n", result2);
-	printf("result3 의 연산 결과 : %d\n", result3);
-	printf("result4 의 연산 결과 : %d\n", result4);
-	printf("result5 의 연산 결과 : %d\n", result5); // <- by YG
-
-/*--------------------------------------------------------------*/
-
-	/*int data = 10;
-	const int constant = 10;
-
-	int result1 = data + data;
-	int result2 = 15 - data;
-	int result3 = constant * 5;
-	int result4 = 15 / 15;
-	int result5 = constant * constant;
-
-	printf("result1 변수의 값 : %d\n", result1);
-	printf("result2 변수의 값 : %d\n", result2);
-	printf("result3 변수의 값 : %d\n", result3);
-	printf("result4 변수의 값 : %d\n", result4);
-	printf("result5 변수의 값 : %d\n", result5); // <- KGS_T */
+	// 표준 출력 함수로 소수를 출력할 때 4byte 크기의
+	// 실수형 자료형은 소수점 이하 6자리까지만 표기하며,
+	// 8byte 크기의 실수형 자료형은 소수점 이하 15자리까지만 표기됩니다.
 
 #pragma endregion
 
-#pragma region 부호 없는 자료형
 
-	/*unsigned short mineral = 65535; // short 자료형의 값의 범위는 65534이지만 unsigned를 사용하면 범위를 더 사용 가능함.
-	unsigned int gas = -1;
-
-	printf("mineral 변수의 값 : %u\n", mineral);
-
-	printf("gas 변수의 값 : %u\n", gas);
-	printf("gas 변수의 값 : %d\n", gas);*/
 #pragma endregion
+
+#pragma region 조건문
+	// 어떤 조건이 주어질 때 해당 조건에 따라
+	// 동작을 수행하도록 실행하는 명령문입니다.
+
+#pragma region 관계연산자
+	// 두 개의 피연산자의 값을 비교하여 그 결과를
+	// 0 또는 1이라는 값으로 나타내는 연산자입니다.
+
+	// int data1 = 10 > 5;
+	// int data2 = 10 < 5;
+	// int data3 = 10 >= 5;
+	// int data4 = 10 <= 5;
+	// int data5 = 10 == 5;
+	// int data6 = 10 != 5;
+	// 
+	// printf("data1의 값 : %d\n", data1);
+	// printf("data2의 값 : %d\n", data2);
+	// printf("data3의 값 : %d\n", data3);
+	// printf("data4의 값 : %d\n", data4);
+	// printf("data5의 값 : %d\n", data5);
+	// printf("data6의 값 : %d\n", data6);
+
+	// 관계 연산자는 조건이 맞을 때 1 이라는 값으로 반환되며,
+	// 조건이 틀릴 때는 0이라는 값으로 반환됩니다.
+#pragma endregion
+
+#pragma region if문
+	// 어떤 특정한 조건을 비교하여 조건이 맞다면
+	// 실행되는 명령입니다.
+
+	// int health = 0;
+	// 
+	// if (health <= 0)
+	// {
+	// 	printf("GameObject Destroy\n");
+	// }
+
+
+#pragma endregion
+
+#pragma region else if문
+	// if문의 조건이 틀릴 때 else if문의 조건이
+	// 맞다면 실행되는 명령문입니다.
+	
+	// char poison = 'Y';
+	// 
+	// if (poison == 'Y')
+	// {
+	// 	printf("독에 중독 되었습니다.\n");
+	// }
+	// else if(poison == 'N')
+	// {
+	// 	printf("정상 상태입니다.\n");
+	// }
+
+	// else if문은 여러 개를 생성하는 것이 가능하며,
+	// if문이 존재하지 않으면 사용할 수 없습니다.
+#pragma endregion
+
+#pragma region else문
+	// if문과 else if문의 조건이 다 틀리면
+	// 실행되는 명령문입니다.
+
+char poison = 'A';
+
+if (poison == 'Y')
+{
+	printf("플레이어가 중독 상태 입니다.\n");
+}
+else if (poison == 'N')
+{
+	printf("플레이어가 정상 상태입니다.\n");
+}
+else (poison == 'A');
+{
+	printf("플레이어가 중독으로 인해 사망하였습니다.\n");
+}
+
+	// if문에 연결된 모든 조건문의 조건이 맞을 때
+	// 가장 위에 있는 조건문만 실행됩니다.
+#pragma endregion
+
+
+#pragma endregion
+
 }
