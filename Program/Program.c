b2502a51b7215aa8ff3c5ac43b8b26a3cc245aed
Yg070212 <winter070212@gmail.com>
2024-12-03 19:46:25 +0900

    24-12-03 <구조체>

--- Program/Program.c
+++ Program/Program.c
@@ -1,6 +1,76 @@
 #include <stdio.h>
+#include <math.h>
+
+struct GameObject
+{
+	char grade;
+	double attack;
+	double defense;
+	int health;
+
+	// 구조체 크기의 경우 멤버 변수의 순서에 따라 메모리의
+	// 크기가 다르게 설정될 수 있으며, 구조체 크기를 결정하는
+	// 형태는 기본 자료형으로만 구성되어 있습니다.
+};
 
 void main()
 {
+#pragma region 구조체
+	// 여러 개의 변수를 하나의 집합으로 구조화한 다음
+	// 하나의 객체를 생성하는 것입니다.
+	
+	// struct GameObject gameObject;
+	// 
+	// gameObject.grade = 'A';
+	// gameObject.health = 75;
+	// gameObject.attack = 5.5f;
+	// gameObject.defense = 1.5f;
+	// 
+	// printf("Grade : %c\nHealth = %d\nAttack : %f\nDefense : %f\n", gameObject.grade, gameObject.health, gameObject.attack, gameObject.defense);
 
+	// 구조체를 선언하기 전에 구조체는 메모리 공간이
+	// 생성되지 않으므로, 구조체 내부에 있는 데이터를
+	// 초기화할 수 없습니다.
+#pragma endregion
+
+#pragma region 바이트 패딩
+	// 멤버 변수를 메모리에서 CPU로 읽을 때 한 번에
+	// 읽을 수 있도록, 컴파일러가 레지스터의 블록에
+	// 맞추어 바이트를 패딩해주는 최적화 작업입니다.
+
+	// struct GameObject object = { 'C', 30, 3.25 };
+	// 
+	// printf("GameObject의 크기 : %d\n", sizeof(object));
+
+	// 구조체의 크기는 구조체를 구성하는 멤버 중에
+	// 크기가 가장 큰 자료형의 배수가 되도록 정렬합니다.
+#pragma endregion
+
+#pragma region 두 점 사이의 거리
+
+	// printf("100의 제곱근 : %lf\n", sqrt(100)); // <- 제곱근
+	// 
+	// printf("10²: %lf\n", pow(10, 2));
+
+	int character1, monster1, character2, monster2;
+	double distance;
+
+	distance = sqrt(pow(character2 - character1, 2) + pow(monster2 - monster1, 2));
+
+	if (distance = 3.0f)
+	{
+		printf("공격 상태");
+	}
+	else if (distance != 3.0f)
+	{
+		printf("공격 해제 상태");
+	}
+	
+
+	
+
+#pragma endregion
+
+
+	return 0;
 }
